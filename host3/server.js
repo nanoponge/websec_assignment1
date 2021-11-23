c7effd757a91922b030ba234ebe0c601ddbd47df
nanoponge <47075318+nanoponge@users.noreply.github.com>
2021-11-23 22:16:45 +0800

    Add files via upload

--- host3/server.js
+++ host3/server.js
@@ -1,1 +1,17 @@
+const express = require('express')
+const { createReadStream } = require('fs')
+const bodyParser = require('body-parser')
+const app = express()
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
+app.get('/', (req, res) => {
+ createReadStream('index.html').pipe(res)
+})
+
+app.get('/api/jsonpdate', (req, res) => {
+ res.jsonp({ date: Date.now() })
+})
+
+
+app.listen(80)
